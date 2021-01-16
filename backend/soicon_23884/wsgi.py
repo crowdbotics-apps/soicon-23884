4d608d7730922cfef67392adbdd094c8d86da2e8
Crowdbotics <team@crowdbotics.com>
2021-01-16 21:24:08 +0000

    Update models

--- backend/soicon_23884/wsgi.py
+++ backend/soicon_23884/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soicon_23884.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soicon_23884.settings")
 
 application = get_wsgi_application()
